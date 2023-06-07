3d6c4a0ffb84018b520ea3fcb2b2eca6ba22e495
naca-nyan <naca.nyan@gmail.com>
2023-06-07 13:13:28 +0900

    :recycle: Refactor weekly.ts

--- weekly.ts
+++ weekly.ts
@@ -1,67 +1,62 @@
-//ver1.0
-//リファレンス
-// https://developers.google.com/apps-script/reference/calendar/calendar
+// ver1.0
+// リファレンス
+// - https://developers.google.com/apps-script/reference/calendar/calendar
 //
 
-const CALENDAR_ID = "カレンダーID"; //カレンダーID
-const WEBHOOK_URL = "ウェブフックのURL"; //ウェブフックのURL
-var weekday = ["日", "月", "火", "水", "木", "金", "土"];
+const CALENDAR_ID = "カレンダーID"; // カレンダーID
+const WEBHOOK_URL = "ウェブフックのURL"; // ウェブフックのURL
 
+// 週間予定通知
+// トリガーによって毎週日曜日に呼び出される
 function notifyWeekly() {
-  //週間予定通知
-
-  var dt = new Date();
-  var message =
-    "来週の天ひま！の予定一覧はこちら↓\n※毎週日曜日に、週間予定をお知らせしています！\n\n";
-
-  //1LP1日分
-  for (var i = 0; i < 7; i++) {
-    dt.setDate(dt.getDate() + 1); //実行日基準で1週間1日ずつセット→JST sun 0：00=UTC sat 15:00だから?
-    message +=
-      Utilities.formatDate(dt, "JST", "★ MM/dd(" + weekday[dt.getDay()] + ")") +
-      "\n"; //日付行
-    let dayText = ""; //予定時刻クリア
-    const calendar = CalendarApp.getCalendarById(CALENDAR_ID); //カレンダーセット
-    const events = calendar.getEventsForDay(dt); //取得日の予定
-
-    const calendarName = calendar.getId(); //カレンダー読み取り
-    if (calendarName == undefined) {
-      //カレンダー全体の予定有無判定
-      message += "予定はありません\n\n";
-      continue; //LP飛ばす
-    }
-
-    //const events = calendar.getEventsForDay(dt);//取得日の予定//←重複排除
-    if (events.length == 0) {
-      //取得日の予定有無判定
-      message += "予定はありません\n\n";
-      continue; //LP飛ばす
-    }
-
-    for (let j = 0; j < events.length; j++) {
-      //取得日の予定全LP
-      dayText += String(
-        Utilities.formatDate(events[j].getStartTime(), "JST", "HH:mm")
-      ); //予定開始時刻
-      dayText += "-";
-      dayText += String(
-        Utilities.formatDate(events[j].getEndTime(), "JST", "HH:mm")
-      ); //予定終了時刻
-      dayText += " ";
-      dayText += String(events[j].getTitle() + "\n"); //予定名
-    }
-
-    dayText += "\n";
-    message += dayText;
+  const calendar = CalendarApp.getCalendarById(CALENDAR_ID);
+  const calendarId = calendar.getId();
+  if (!calendarId) {
+    console.error("cannot get calendar id");
+    return;
   }
 
-  //JSON用プレフィックス追加
+  const format = (date, format_string) =>
+    Utilities.formatDate(date, "JST", format_string);
+  const dayOfWeek = (date) => {
+    const daysOfWeek = ["日", "月", "火", "水", "木", "金", "土"];
+    return daysOfWeek[date.getDay()];
+  };
+
+  let message = `\
+来週の天ひま！の予定一覧はこちら↓
+※毎週日曜日に、週間予定をお知らせしています！
+
+`;
+
+  for (let i = 0; i < 7; i++) {
+    const date = new Date();
+    date.setDate(date.getDate() + i + 1); // 次の日からなので + 1
+
+    message += `\
+★ ${format(date, "MM/dd")}(${dayOfWeek(date)})
+`;
+
+    const events = calendar.getEventsForDay(date);
+    let dayText = "";
+    if (events.length == 0) {
+      dayText = "予定はありません";
+    } else {
+      for (const e of events) {
+        const start = format(e.getStartTime(), "HH:mm");
+        const end = format(e.getEndTime(), "HH:mm");
+        const title = e.getTitle();
+        dayText += `${start}-${end} ${title}`;
+      }
+    }
+    message += dayText + "\n\n";
+  }
+
   const payload = {
-    username: `天ひまリマインダー`,
+    username: "天ひまリマインダー",
     content: message,
   };
 
-  //JSON梱包&発送
   UrlFetchApp.fetch(WEBHOOK_URL, {
     method: "post",
     contentType: "application/json",
